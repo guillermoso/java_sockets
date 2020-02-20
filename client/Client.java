ee84daec5cf2f2078c3397166ac3354c724fdcc9
Guillermo Alvarez <gjalvarez09@gmail.com>
2020-02-19 17:33:32 -0700

    needs socket implementations

--- client/Client.java
+++ client/Client.java
@@ -1,3 +1,5 @@
+package client;
+
 import java.io.*;
 import java.net.*;
 import java.util.concurrent.TimeUnit;
@@ -24,10 +26,11 @@ public class Client {
 
             switch (selectedOption) {
                 case 1:
-                    
+                    client.queryMovie(client.getQueryFromUser());
                     break;
                 case 2:
                     exitFlag = true;
+                    break;
                 default:
                     System.out.println("Invalid option.");
                     sleep();
@@ -38,10 +41,9 @@ public class Client {
 
     private int mainMenu() throws Exception {
         clearConsole();
-        System.out.println("\t*** Query movie's data ***");
-        System.out.println("");
-        System.out.println("Select an option:");
-        System.out.println("1. Make a query");
+        System.out.println("\t*** Query a movie's data ***\n");
+        System.out.println("Select an option.");
+        System.out.println("1. Make a query about a movie");
         System.out.println("2. exit");
         System.out.print("Option:  ");
         String input = System.console().readLine();
@@ -56,9 +58,33 @@ public class Client {
         return selectedOption;
     }
 
+    private String getQueryFromUser() {
+        clearConsole();
+
+        System.out.println("\t*** Query a movie's data **\n");
+        System.out.print("Enter the name of the movie you want to query about:  ");
+        String moviesName = System.console().readLine();
+        System.out.println("\nSelect the option you want to query.");
+        System.out.println("1. Genre");
+        System.out.println("2. Lead studio");
+        System.out.println("3. Audience score");
+        System.out.println("4. Profitability");
+        System.out.println("5. Rotten tomatoes score");
+        System.out.println("6. Worldwide gross");
+        System.out.println("7. Year");
+        System.out.print("Option:  ");
+        String opCode = System.console().readLine();
+
+        return opCode + "|" + moviesName;
+    }
+
+    private String queryMovie(String query) {
+
+    }
+
     private static void sleep() {
         try {
-            TimeUnit.SECONDS.sleep(2);
+            TimeUnit.SECONDS.sleep(3);
         } catch (InterruptedException e2) {
             System.out.println(e2);
         }
@@ -71,7 +97,8 @@ public class Client {
             if (OS.contains("Windows")) {
                 Runtime.getRuntime().exec("cls");
             } else {
-                Runtime.getRuntime().exec("clear");
+                // Runtime.getRuntime().exec("clear");
+                System.out.print("\033[H\033[2J");
             }
         } catch (Exception e) {
             System.out.println(e);
